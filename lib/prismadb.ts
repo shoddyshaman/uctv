111e19b623de04167133a521951159232a1e2f25
shoddyshaman <dhananjoy.tiwari@gmail.com>
2023-10-09 23:45:15 -0700

    added postinstall script

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -8,3 +8,20 @@ const client = globalThis.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client
 
 export default client
+
+// import { PrismaClient } from '@prisma/client'
+
+// // Avoid instantiating too many instances of Prisma in development
+// // https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices#problem
+// let prisma
+
+// if (process.env.NODE_ENV === 'production') {
+//   prisma = new PrismaClient()
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient()
+//   }
+//   prisma = global.prisma
+// }
+
+// export default prisma
